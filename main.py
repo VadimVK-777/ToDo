d87aebae394f343c9b77a62237607eef272198d9
VADIM <investorprof@gmail.com>
2025-03-31 15:09:12 +0500

    Реализовал полноценную работу с аргументами коммандной строки за исключение тегов.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from pickle import GLOBAL
 from pprint import pprint as pp
 import csv
+from sys import argv
 
 # TODO handle "next friday"
 # TODO изучаем ооп
@@ -32,7 +33,6 @@ def update_data() -> dict:
                 record['task'] = record_data[4]
                 records.append(record)
             database['records'] = records
-            # pp(database)
             return database
     else:
         with open(TODO_DB_FILE, 'w', encoding='utf-8') as f:
@@ -52,24 +52,18 @@ def start_help() -> None:
           '7 - Удалить запись\n'
           '8 - Посмотреть все заметки(table)')
 
-def add_todo(date_time, note, daily) -> list: #TODO проверка правильности ввода данных юзером
-    new_string = []
-    new_string.extend([date_time, daily, note])
-    list_data.append(new_string)
-    list_data.sort()
-    return list_data
 
-# TODO Удалить в будущем
-def data_show(database: dict) -> None:
-    for index,key in enumerate(database['records'], 1):
-        if key['daily']:
-            print(f'{index}. [{key['date'].date()} --:--] - {key['task']}')
-        else:
-            print(f'{index}. [{key['date'].strftime("%Y-%m-%d %H:%M")}] - {key['task']}')
+def add_todo(database: dict, date, time, note, daily) -> list:  # TODO проверка правильности ввода данных юзером
+    date_time = datetime.strptime(f'{date} {time}', "%Y-%m-%d %H:%M")
+    record = {}
+    record['date'] = date_time
+    record['daily'] = bool(int(daily))
+    record['task'] = note
+    record['tags'] = ['0']
+    database['records'].append(record)
+    return database
 
 def read_todo_day(database: dict, date_todo: datetime) -> dict:
-    # pp(database)
-    # pp(date_todo)
     tags = database['tags']
     records = []
     for i in database['records']:
@@ -86,39 +80,35 @@ def read_todo_day(database: dict, date_todo: datetime) -> dict:
         database['records'] = records
     return database
 
-def read_todo_daytime_or_diapazon(database: dict, start_date: datetime=None, finish_date: datetime=None) -> dict:
+def my_records(dictionary: dict) -> dict:
+    record = {}
+    record['date'] = dictionary['date']
+    record['daily'] = dictionary['daily']
+    record['tags'] = dictionary['tags']
+    record['task'] = dictionary['task']
+    return record
+
+def read_todo_daytime_or_diapazon(database: dict, daily=0, start_date: datetime = None, finish_date: datetime = None) -> dict:
     records = []
     tags = database['tags']
-    if finish_date is None:
+    if finish_date is None and daily == 1:
         for i in database['records']:
-            record = {}
             if i['date'].date() == start_date.date():
-                # date_time = datetime.strptime(f'{record_data[0]} {record_data[1]}', "%Y-%m-%d %H:%M")
-                record['date'] = i['date']
-                record['daily'] = i['daily']
-                record['tags'] = i['tags']
-                record['task'] = i['task']
-                records.append(record)
-        database = {}
-        database['tags'] = tags
-        database['records'] = records
-        return database
-    else:
+                records.append(my_records(i))
+    elif finish_date is None and daily == 0:
         for i in database['records']:
-            record = {}
+            if i['date'] == start_date:
+                records.append(my_records(i))
+    elif finish_date:
+        for i in database['records']:
             if start_date <= i['date'] <= finish_date:
-                record['date'] = i['date']
-                record['daily'] = i['daily']
-                record['tags'] = i['tags']
-                record['task'] = i['task']
-                records.append(record)
-        database = {}
-        database['tags'] = tags
-        database['records'] = records
-        return database
+                records.append(my_records(i))
+    database = {}
+    database['tags'] = tags
+    database['records'] = records
+    return database
 
 def show_compact(database: dict) -> None:
-    # pp(database['records'])
     # Создаем словарь для группировки по датам
     grouped_data = defaultdict(list)
     # Группируем данные по дате (без учета времени)
@@ -143,7 +133,6 @@ def show_compact(database: dict) -> None:
         print('.............\n')
 
 def show_short1(database: dict) -> None:
-    # pp(database['records'])
     # Создаем словарь для группировки по датам
     grouped_data = defaultdict(list)
     # Группируем данные по дате (без учета времени)
@@ -165,40 +154,7 @@ def show_short1(database: dict) -> None:
             for line in list_task_daily:
                 print(f'{"*":>6} {"|":>5} {line["task"]}')
 
-
-
-# def show_table(database: dict) -> None:
-#     # Создаем словарь для группировки по датам
-#     grouped_data = defaultdict(list)
-#     # Группируем данные по дате (без учета времени)
-#     header = "+------------+---------------------------------+---------------------------------+"
-#     date_row = "|  Date      |              Task               |           Daily Task            |"
-#     time_row = "|     Time   |                                 |                                 |"
-#     print(f'{header}\n{date_row}\n{time_row}\n{header}')
-#     for entry in database['records']:
-#         date_key = entry['date'].date()  # Получаем только дату
-#         grouped_data[date_key].append(entry)
-#     # Преобразуем обратно в список вложенных списков
-#     result = list(grouped_data.items())
-#     for date_key, list_day in result:
-#         # Задачи с daily: False
-#         list_task_time = [x for x in list_day if x['daily'] is False]
-#         # Задачи с daily: True
-#         list_task_daily = [x for x in list_day if x['daily'] is True]
-#         # Выводим дату
-#         print(f'| {date_key} | {"":>31} | {"":>32}|')
-#         # Определяем максимальное количество задач в день
-#         max_length = max(len(list_task_time), len(list_task_daily))
-#         for i in range(max_length):
-#             time_task = list_task_time[i] if i < len(list_task_time) else {'date': None, 'task': ''}
-#             daily_task = list_task_daily[i] if i < len(list_task_daily) else {'task': ''}
-#             time_str = time_task['date'].strftime("%H:%M") if time_task['date'] else ''
-#             task_str = truncate_task(time_task["task"])  # Обрезаем задачу
-#             daily_str = truncate_task(daily_task["task"])  # Обрезаем ежедневную задачу
-#             print(f'| {time_str:>10} | {task_str:<31} | {daily_str:<31} |')
-#         print(header)
-
-def show_table(database: dict, len_task=11, len_daily=19) -> None:
+def show_table(database: dict, len_task=40, len_daily=40) -> None:
     def truncate_task(task: str, max_length: int) -> str:
         """Обрезает задачу, если она превышает max_length, добавляя многоточие."""
         if len(task) > max_length:
@@ -207,9 +163,9 @@ def show_table(database: dict, len_task=11, len_daily=19) -> None:
     # Создаем словарь для группировки по датам
     grouped_data = defaultdict(list)
     # Группируем данные по дате (без учета времени)
-    header = f"+------------+{'-'*(len_task+2)}+{'-'*(len_daily+2)}+"
-    date_row = f"|  Date      |{' '*(len_task//2)}Task{' '*(len_task-(len_task//2)-2)}|{' '*(len_daily//2 -4)}Daily Task{' '*(len_daily-(len_daily//2)-4)}|"
-    time_row = f"|     Time   |  {' '*(len_task)}|  {' '*(len_daily)}|"
+    header = f"+------------+{'-' * (len_task + 2)}+{'-' * (len_daily + 2)}+"
+    date_row = f"|  Date      |{' ' * (len_task // 2)}Task{' ' * (len_task - (len_task // 2) - 2)}|{' ' * (len_daily // 2 - 4)}Daily Task{' ' * (len_daily - (len_daily // 2) - 4)}|"
+    time_row = f"|     Time   |  {' ' * (len_task)}|  {' ' * (len_daily)}|"
     print(f'{header}\n{date_row}\n{time_row}\n{header}')
     for entry in database['records']:
         date_key = entry['date'].date()  # Получаем только дату
@@ -236,7 +192,6 @@ def show_table(database: dict, len_task=11, len_daily=19) -> None:
         print(header)
 
 def show_long(database: dict) -> None:
-    # pp(database['records'])
     # Создаем словарь для группировки по датам
     grouped_data = defaultdict(list)
     # Группируем данные по дате (без учета времени)
@@ -283,8 +238,14 @@ def del_todo(database, del_dict: dict) -> None:
     database['records'] = result
     return database
 
+def save_to_file(database: dict) -> None:
+    with open(TODO_DB_FILE, 'w', encoding='utf-8') as f:
+        print('-'.join(database['tags']), file=f)
+        for string in database['records']:
+            f.write(
+                f'{string['date'].strftime("%Y-%m-%d %H:%M")} {int(string['daily'])} {'/'.join(string['tags'])} {string['task']}' + '\n')
 
-def main() -> None:
+def interactive_mode() -> None:
     esc1 = '1'
     database = update_data()
     while esc1 != '0':
@@ -296,28 +257,25 @@ def main() -> None:
         if comm1 == 1:
             # Получаем текущую дату
             default_date = datetime.now()
-            print(f'130 {default_date}')
             # Запрашиваем ввод пользователя
-            user_input = input(f"Введите дату в формате '{default_date.date()}'. Если запись на сегодня, нажмите Enter: ")
+            user_input = input(
+                f"Введите дату в формате '{default_date.date()}'. Если запись на сегодня, нажмите Enter: ")
             # Если пользователь не ввел ничего, используем текущую дату
             if not user_input:
-                user_date = default_date
+                user_date = default_date.strftime("%Y-%m-%d")
             else:
-                # Пробуем преобразовать введенную дату в объект datetime
-                try:
-                    user_date = datetime.strptime(user_input, "%Y-%m-%d").date()
-                except ValueError:
-                    print("Некорректный формат даты. Используйте 'YYYY-MM-DD'.")
-                    user_date = default_date  # Используем значение по умолчанию в случае ошибки
+                user_date = user_input
             time_todo = input('Если задача на весь день, нажмите Enter. Если нет, напишите время в форматe "12:00": ')
             if not time_todo:
                 ask_todo = input('Введите дату в формате: "Встреча с командой" - ')  # Текст события
-                add_todo(user_date, ask_todo, '1')
+                date1 = user_date
+                time1 = '00:00'
+                database = add_todo(database, date1, time1, ask_todo, '1')
             else:
-                time_todo = datetime.strptime(time_todo, "%H:%M").time()
-                my_time = datetime.combine(user_date, time_todo)
+                date1 = user_date
+                time1 = time_todo
                 ask_todo = input('Введите дату в формате: "Встреча с командой" - ')  # Текст события
-                add_todo(my_time, ask_todo, '0')
+                database = add_todo(database, date1, time1, ask_todo, '0')
         elif comm1 == 2:
             show_long(database)
         elif comm1 == 3:
@@ -327,13 +285,13 @@ def main() -> None:
         elif comm1 == 5:
             date2 = input('Введите дату записи в формате: "2023-10-01 - ')
             date_time = datetime.strptime(f'{date2}', "%Y-%m-%d")
-            show_table(read_todo_daytime_or_diapazon(database, date_time))
+            show_table(read_todo_daytime_or_diapazon(database, 1, date_time))
         elif comm1 == 6:
             start1 = input('Введите дату начала диапазона в формате: "2023-10-01 - ')
             finish1 = input('Введите дату конца диапазона в формате: "2023-10-01 - ')
             start_date = datetime.strptime(start1, "%Y-%m-%d")
             end_date = datetime.strptime(finish1, "%Y-%m-%d")
-            show_table(read_todo_daytime_or_diapazon(database, start_date, end_date))
+            show_table(read_todo_daytime_or_diapazon(database, 1, start_date, end_date))
         elif comm1 == 7:
             date2 = input('Введите дату записи, которую нужно удалить в формате: "2023-10-01 - ')
             time2 = input('Введите время записи, которую нужно удалить в формате: "14:30" - ')
@@ -346,11 +304,77 @@ def main() -> None:
         elif comm1 == 8:
             show_table(database)
         esc1 = input('\nДля продолжения работы программы нажмите Enter.\nДля завершения напишите 0')
-    # print(list_data)
-    with open(TODO_DB_FILE, 'w', encoding='utf-8') as f:
-        print('-'.join(database['tags']), file=f)
-        for string in database['records']:
-            f.write(f'{string['date'].strftime("%Y-%m-%d %H:%M")} {int(string['daily'])} {'/'.join(string['tags'])} {string['task']}' + '\n')
+    save_to_file(database)
+
+args = argv[1:]
+
+def popit(args):
+    args.pop(0)
+    args.pop(0)
+
+def del_todo2(database, day, task) -> None:
+    tags = database['tags']
+    pp(database)
+    result = [item for item in database['records'] if item['task'] != task or item['date'].date() != day]
+    database = {}
+    database['tags'] = tags
+    database['records'] = result
+    pp(database)
+    return database
+
+def date_10(str10: str) -> datetime:
+    return datetime.strptime(str10, "%Y-%m-%d")
+
+def date_16(str16: str) -> datetime:
+    return datetime.strptime(f'{str16[:10]} {str16[-5:]}', "%Y-%m-%d %H:%M")
+
+def main():
+    database = update_data()
+    while args:
+        arg = args[0]
+        if arg == '-i' or arg == '--interactive':
+            interactive_mode()
+        elif arg == '-v' or arg == '--view':
+            if args[1] == 'short':
+                show_short1(database)
+            elif args[1] == 'compact':
+                show_compact(database)
+            elif args[1] == 'long':
+                show_long(database)
+            elif args[1] == 'table':
+                show_table(database)
+        elif arg == '-r' or arg == '--remove':
+            day1 = datetime.strptime(args[1], "%Y-%m-%d").date()
+            task1 = ' '.join(args[2:])
+            save_to_file(del_todo2(database, day1, task1))
+        elif arg == '-a' or arg == '--add':
+            if len(args[1]) == 10:
+                database = add_todo(database, args[1], '00:00', args[2], '1')
+                save_to_file(database)
+            elif len(args[1]) == 16:
+                database = add_todo(database, args[1][:10], args[1][-5:], args[2], '0')
+                save_to_file(database)
+        elif arg == '-l' or arg == '--list':
+            if len(args) < 3:
+                if len(args[1]) == 10:
+                    start_date = datetime.strptime(args[1], "%Y-%m-%d")
+                    show_table(read_todo_daytime_or_diapazon(database, 1, start_date))
+                elif len(args[1]) == 16:
+                    start_date = datetime.strptime(f'{args[1][:10]} {args[1][-5:]}', "%Y-%m-%d %H:%M")
+                    show_table(read_todo_daytime_or_diapazon(database,0, start_date))
+            else:
+                if len(args[1]) == 10 and len(args[2]) == 10:
+                    show_table(read_todo_daytime_or_diapazon(database, 1, date_10(args[1]), date_10(args[2])))
+                elif len(args[1]) == 10 and len(args[2]) == 16:
+                    show_table(read_todo_daytime_or_diapazon(database, 0, date_10(args[1]), date_16(args[2])))
+                elif len(args[1]) == 16 and len(args[2]) == 16:
+                    show_table(read_todo_daytime_or_diapazon(database, 0, date_16(args[1]), date_16(args[2])))
+                elif len(args[1]) == 16 and len(args[2]) == 10:
+                    show_table(read_todo_daytime_or_diapazon(database, 0, date_16(args[1]), date_10(args[2])))
+
+        args.clear()
+
+# interactive_mode()
 
 if __name__ == "__main__":
     main()
